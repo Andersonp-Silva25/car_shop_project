e5dd76f2ba6b725443e3cccae44b780075cd4434
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-02-13 19:38:11 -0300

    :sparkles: Feat: Criando a rota /cars para cadastrar um novo carro

---
+++ src/Domains/Car.ts
@@ -0,0 +1,89 @@
+import ICar from '../Interfaces/ICar';
+
+class Car {
+  protected id: string | undefined;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status: boolean | undefined;
+  protected buyValue: number;
+  private doorsQty: number;
+  private seatsQty: number;
+
+  constructor(cars: ICar) {
+    this.id = cars.id;
+    this.model = cars.model;
+    this.year = cars.year;
+    this.color = cars.color;
+    this.status = cars.status || false;
+    this.buyValue = cars.buyValue;
+    this.doorsQty = cars.doorsQty;
+    this.seatsQty = cars.seatsQty;
+  }
+
+  public getId() {
+    return this.id;
+  }
+
+  public setId(id: string) {
+    this.id = id;
+  }
+  
+  public getModel() {
+    return this.model;
+  }
+
+  public setModel(model: string) {
+    this.model = model;
+  }
+
+  public getYear() {
+    return this.year;
+  }
+
+  public setYear(year: number) {
+    this.year = year;
+  }
+
+  public getColor() {
+    return this.color;
+  }
+
+  public setColor(color: string) {
+    this.color = color;
+  }
+
+  public getStatus() {
+    return this.status;
+  }
+
+  public setStatus(status: boolean) {
+    this.status = status;
+  }
+
+  public getBuyValue() {
+    return this.buyValue;
+  }
+
+  public setBuyValue(buyValue: number) {
+    this.buyValue = buyValue;
+  }
+
+  public getDoorsQty() {
+    return this.doorsQty;
+  }
+
+  public setDoorsQty(doorsQty: number) {
+    this.doorsQty = doorsQty;
+  }
+
+  public getSeatsQty() {
+    return this.seatsQty;
+  }
+
+  public setSeatsQty(seatsQty: number) {
+    this.seatsQty = seatsQty;
+  }
+}
+
+export default Car;

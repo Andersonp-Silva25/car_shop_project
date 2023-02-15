5b87c5c0becf886cce6889edd4e78c76993ffb82
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-02-14 21:12:40 -0300

    :rotating_light: Tests: Criando testes para cobrir 80% da camada Service

--- tests/unit/Services/Mocks/MotorMock.ts
+++ tests/unit/Services/Mocks/MotorMock.ts
@@ -10,4 +10,38 @@ const newMotor: IMotorcycle = {
   engineCapacity: 600,
 };
 
-export default newMotor;
+const listMotor: IMotorcycle[] = [
+  {
+    id: '634852326b35b59438fbea2f',
+    model: 'Honda Cb 600f Hornet',
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30.000,
+    category: 'Street',
+    engineCapacity: 600,
+  },
+  {
+    id: '634852326b35b59438fbea31',
+    model: 'Honda Cbr 1000rr',
+    year: 2011,
+    color: 'Orange',
+    status: true,
+    buyValue: 59.900,
+    category: 'Street',
+    engineCapacity: 1000,
+  },
+];
+
+const updatedMotor: IMotorcycle = {
+  id: '634852326b35b59438fbea2f',
+  model: 'Cg 160 Titan',
+  year: 2005,
+  color: 'Black',
+  status: true,
+  buyValue: 30.000,
+  category: 'Street',
+  engineCapacity: 600,
+};
+
+export { newMotor, listMotor, updatedMotor };
--- tests/unit/Services/Motorcycle.test.ts
+++ tests/unit/Services/Motorcycle.test.ts
@@ -3,7 +3,7 @@ import Sinon from 'sinon';
 import { Model } from 'mongoose';
 import MotorService from '../../../src/Services/MotorService';
 import Motorcycle from '../../../src/Domains/Motorcycle';
-import newMotor from './Mocks/MotorMock';
+import { listMotor, newMotor, updatedMotor } from './Mocks/MotorMock';
 
 describe('Testando as funcionalidade de MotorService', function () {
   it('Deve cadastrar uma nova moto', async function () {
@@ -20,7 +20,45 @@ describe('Testando as funcionalidade de MotorService', function () {
 
     // Assert
     expect(result).to.be.deep.equal(response);
+  });
 
+  it('Deve listar todas as motos', async function () {
+    // Arrange
+    Sinon.stub(Model, 'find').resolves(listMotor);
+
+    // Action
+    const service = new MotorService();
+    const result = await service.find();
+
+    // Assert
+    expect(result).to.be.deep.equal(listMotor);
+  });
+
+  it('Deve buscar uma moto pelo seu id', async function () {
+    // Arrange
+    Sinon.stub(Model, 'findById').resolves(listMotor[0]);
+
+    // Action
+    const service = new MotorService();
+    const result = await service.findById('634852326b35b59438fbea2f');
+        
+    // Assert
+    expect(result).to.be.deep.equal(listMotor[0]);
+  });
+
+  it('Deve atualizar uma moto pelo seu id', async function () {
+    // Arrange
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(updatedMotor);
+
+    // Action
+    const service = new MotorService();
+    const result = await service.update('634852326b35b59438fbea2f', newMotor);
+        
+    // Assert
+    expect(result).to.be.deep.equal(updatedMotor);
+  });
+
+  afterEach(function () {
     Sinon.restore();
   });
 });
